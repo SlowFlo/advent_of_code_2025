8c36c62e49a85f09d94ab1d3c6f4d93b7fb0004a
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-08 15:39:43 +0100

    feat: add main to get the problem 'sanswer

--- Day 2 - Gift Shop/part 1/invalid_ids.py
+++ Day 2 - Gift Shop/part 1/invalid_ids.py
@@ -1,3 +1,8 @@
+import itertools
+
+from utils import default_input_path, read_input_lines
+
+
 def range_2_numbers(str_range: str) -> tuple[int]:
     beginning, end = str_range.split("-")
     return tuple(range(int(beginning), int(end) + 1))
@@ -17,3 +22,14 @@ def invalid_ids_in_range(str_range: str) -> tuple[int, ...]:
     numbers_in_range = range_2_numbers(str_range)
 
     return tuple(x for x in numbers_in_range if not is_id_valid(str(x)))
+
+
+if __name__ == "__main__":
+    path = default_input_path()
+    ranges = read_input_lines(path)[0].split(",")
+
+    invalid_ids_sum = sum(
+        # flatten the iterable (from_iterable(map...) => flatMap in JS)
+        itertools.chain.from_iterable(map(invalid_ids_in_range, ranges))
+    )
+    print(invalid_ids_sum)
