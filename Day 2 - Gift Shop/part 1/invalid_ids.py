a0928895b8fd786a4ce91b5810b387f5761b172f
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-08 14:28:28 +0100

    feat: is_id_valid()

--- Day 2 - Gift Shop/part 1/invalid_ids.py
+++ Day 2 - Gift Shop/part 1/invalid_ids.py
@@ -1,3 +1,7 @@
 def range_2_numbers(str_range: str) -> tuple[int]:
     beginning, end = str_range.split("-")
     return tuple(range(int(beginning), int(end) + 1))
+
+
+def is_id_valid(id: int) -> bool:
+    return True
