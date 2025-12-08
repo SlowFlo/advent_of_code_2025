926688046ac80b983f3300f8d294df1e757e4dc6
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-08 10:31:36 +0100

    feat: test_range_2_numbers_1_3_is_1_2_3

--- Day 2 - Gift Shop/part 1/test_invalid_ids.py
+++ Day 2 - Gift Shop/part 1/test_invalid_ids.py
@@ -9,3 +9,7 @@ def test_range_2_numbers_1_1_is_1():
 
 def test_range_2_numbers_1_2_is_1_2():
     assert range_2_numbers("1-2") == (1, 2)
+
+
+def test_range_2_numbers_1_3_is_1_2_3():
+    assert range_2_numbers("1-3") == (1, 2, 3)
