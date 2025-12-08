1546ea2de36f49a9ed521c7ff143252bd1927e47
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-08 14:27:45 +0100

    test: parametrize test_range_2_numbers() and test_is_id_valid

--- Day 2 - Gift Shop/part 1/test_invalid_ids.py
+++ Day 2 - Gift Shop/part 1/test_invalid_ids.py
@@ -1,14 +1,22 @@
 import pytest
 
-from invalid_ids import range_2_numbers
+from invalid_ids import range_2_numbers, is_id_valid
 
 
-def test_range_2_numbers_1_1_is_1():
-    assert range_2_numbers("1-1") == (1,)
+@pytest.mark.parametrize(
+    ["str_range", "result"],
+    [
+        ["1-1", (1,)],
+        ["1-2", (1, 2)],
+        ["1-3", (1, 2, 3)],
+    ],
+)
+def test_range_2_numbers(str_range, result):
+    assert range_2_numbers(str_range) == result
 
 
-def test_range_2_numbers_1_2_is_1_2():
-    assert range_2_numbers("1-2") == (1, 2)
+def test_is_id_valid():
+    assert is_id_valid(1)
 
 
 def test_range_2_numbers_1_3_is_1_2_3():
