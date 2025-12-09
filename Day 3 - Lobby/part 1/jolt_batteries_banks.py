8d08eb198a3eeab6f7d09a6ac618e73c751e648e
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-09 15:11:48 +0100

    test: test_find_2_first_biggest_numbers_in_bank()

---
+++ Day 3 - Lobby/part 1/jolt_batteries_banks.py
@@ -0,0 +1,12 @@
+from utils import default_input_path, read_input_lines
+
+
+def find_2_first_biggest_numbers_in_bank(bank: str) -> str:
+    pass
+
+
+if __name__ == "__main__":
+    path = default_input_path()
+    banks = read_input_lines(path)
+
+    print(banks)
