87809c03a951be741eda8bf174224172eceed384
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-10 17:44:04 +0100

    feat: nb_roll_papers_accessible()

---
+++ Day 4 - Printing Department/part 1/forklift_accessibility.py
@@ -0,0 +1,12 @@
+from utils import default_input_path, read_input_lines
+
+
+def nb_roll_papers_accessible(grid: str) -> int:
+    return 1
+
+
+if __name__ == "__main__":
+    path = default_input_path()
+    lines = read_input_lines(path)
+
+    print(lines)
---
+++ Day 4 - Printing Department/part 1/test_forklift_accessibility.py
@@ -0,0 +1,18 @@
+import pytest
+
+from forklift_accessibility import *
+
+
+@pytest.mark.parametrize(
+    ["grid", "result"],
+    [
+        [
+            """...
+.@.
+...""",
+            1,
+        ],
+    ],
+)
+def test_find_2_first_biggest_numbers_in_bank(grid, result):
+    assert nb_roll_papers_accessible(grid) == result
