ded3a4d18939f17fc147560c803bcbb5e1ba8916
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-11 19:05:04 +0100

    feat: count_roll_papers_accessible_in_middle_row

--- Day 4 - Printing Department/part 1/forklift_accessibility.py
+++ Day 4 - Printing Department/part 1/forklift_accessibility.py
@@ -1,8 +1,45 @@
 from utils import default_input_path, read_input_lines
 
 
-def nb_roll_papers_accessible(grid: str) -> int:
+def total_roll_papers_accessible(grid: str) -> int:
     return grid.count("@")
+
+
+def count_roll_papers_accessible_in_middle_row(
+    upper_row: str, middle_row: str, lower_row: str
+) -> int:
+    result = 0
+    for [i, c] in enumerate(middle_row):
+        if i == 0:
+            start = 0
+            middle_row_slice = slice(i + 1, i + 2)
+        else:
+            start = i - 1
+            middle_row_slice = slice(i - 1, i + 2, 2)
+
+        print(
+            upper_row[start : i + 2],
+            upper_row[start : i + 2].count("@"),
+        )
+        print(
+            middle_row[middle_row_slice],
+            middle_row[middle_row_slice].count("@"),
+        )
+        print(
+            lower_row[start : i + 2],
+            lower_row[start : i + 2].count("@"),
+        )
+        print()
+        if (
+            c == "@"
+            and upper_row[start : i + 2].count("@")
+            + middle_row[middle_row_slice].count("@")
+            + lower_row[start : i + 2].count("@")
+            < 4
+        ):
+            result += 1
+
+    return result
 
 
 if __name__ == "__main__":
--- Day 4 - Printing Department/part 1/test_forklift_accessibility.py
+++ Day 4 - Printing Department/part 1/test_forklift_accessibility.py
@@ -1,6 +1,9 @@
 import pytest
 
-from forklift_accessibility import *
+from forklift_accessibility import (
+    total_roll_papers_accessible,
+    count_roll_papers_accessible_in_middle_row,
+)
 
 
 @pytest.mark.parametrize(
@@ -32,5 +35,22 @@ from forklift_accessibility import *
         ],
     ],
 )
-def test_find_2_first_biggest_numbers_in_bank(grid, result):
-    assert nb_roll_papers_accessible(grid) == result
+def test_total_roll_papers_accessible(grid, result):
+    assert total_roll_papers_accessible(grid) == result
+
+
+@pytest.mark.parametrize(
+    ["upper_row", "middle_row", "lower_row", "result"],
+    [
+        ["..@", ".@@", "..@", 2],
+        [".@@", ".@@", "..@", 0],
+        [".@@", "@@@", "..@", 1],
+    ],
+)
+def test_count_roll_papers_accessible_in_middle_row(
+    upper_row, middle_row, lower_row, result
+):
+    assert (
+        count_roll_papers_accessible_in_middle_row(upper_row, middle_row, lower_row)
+        == result
+    )
